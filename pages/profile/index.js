f84c0757b75b1495c2de7f73623ac39ded5164d4
Abhinaba De <deabhinaba@gmail.com>
2023-05-16 10:23:35 +0530

    profile

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,9 +3,17 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 import React from 'react'
 
 const Profile = () => {
+
+  const { user, error, isLoading } = useUser();
+
   return (
-    <div>
-      
+    <div className='profile'>
+      <img src={user.picture} height={150} width={150}/>
+      <h3>Name: {user.name}</h3>
+      <h3>Email: {user.email}</h3>
+      <h5>Nickname: {user.nickname}</h5>
+      <p>Session ID: {user.sid}</p>
+      <p>Updated at: {user.updated_at}</p>
     </div>
   )
 }
