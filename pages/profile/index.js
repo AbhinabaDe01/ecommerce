3c4abab47e9d14a4eef847c569df20cab038cfc1
Abhinaba De <deabhinaba@gmail.com>
2023-05-16 10:32:36 +0530

    profile

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -4,22 +4,27 @@ import React from 'react'
 
 const Profile = () => {
 
-  const { user, error, isLoading } = useUser();
+  // const { user, error, isLoading } = useUser();
 
-  if(user && user.given_name){
-    return(
-      <div className='profile'>
-        <img src={user.picture} height={150} width={150}/>
-        <h3>Name: {user.name}</h3>
-        <h3>Email: {user.email}</h3>
-        <h5>Nickname: {user.nickname}</h5>
-        <p>Session ID: {user.sid}</p>
-        <p>Updated at: {user.updated_at}</p>
-      </div>
-    )
-  } else {
-    <p>Other info</p>
-  }
+  // if(user && user.given_name){
+  //   return(
+  //     <div className='profile'>
+  //       <img src={user.picture} height={150} width={150}/>
+  //       <h3>Name: {user.name}</h3>
+  //       <h3>Email: {user.email}</h3>
+  //       <h5>Nickname: {user.nickname}</h5>
+  //       <p>Session ID: {user.sid}</p>
+  //       <p>Updated at: {user.updated_at}</p>
+  //     </div>
+  //   )
+  // } else {
+  //   <p>Other info</p>
+  // }
+
+  return (
+    <>
+    </>
+  )
 }
 
 export default Profile
