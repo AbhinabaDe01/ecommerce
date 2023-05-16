045ed6a3bbb91acc529d71cf01263375be38fe44
Abhinaba De <deabhinaba@gmail.com>
2023-05-16 10:29:14 +0530

    profile 2

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -6,16 +6,20 @@ const Profile = () => {
 
   const { user, error, isLoading } = useUser();
 
-  return (
-    <div className='profile'>
-      <img src={user.picture} height={150} width={150}/>
-      <h3>Name: {user.name}</h3>
-      <h3>Email: {user.email}</h3>
-      <h5>Nickname: {user.nickname}</h5>
-      <p>Session ID: {user.sid}</p>
-      <p>Updated at: {user.updated_at}</p>
-    </div>
-  )
+  if(user.given_name){
+    return(
+      <div className='profile'>
+        <img src={user.picture} height={150} width={150}/>
+        <h3>Name: {user.name}</h3>
+        <h3>Email: {user.email}</h3>
+        <h5>Nickname: {user.nickname}</h5>
+        <p>Session ID: {user.sid}</p>
+        <p>Updated at: {user.updated_at}</p>
+      </div>
+    )
+  } else {
+    <p>Other info</p>
+  }
 }
 
 export default Profile
